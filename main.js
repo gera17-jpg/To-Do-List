abbf5c216b651738f66be1b7da1cbf0f194b6b4d
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2023-11-13 18:56:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 
 const tasks = [];
+let tasksHTML = '';
+
+function updateTasks() {
+    tasksHTML = '';
+    for (let i = 0; i < tasks.length; i++) {
+        const toDo = tasks[i];
+        const html = `<p>${toDo}</p>`;
+        tasksHTML += html;
+    }
+    console.log(tasksHTML);
+}
+
+document.querySelector('.js-todolist') 
 
 function addToDo() {
     const taskInput = document.querySelector('.input-value');
@@ -7,4 +20,9 @@ function addToDo() {
 
     tasks.push(name);
     console.log(tasks);
+
+    taskInput.value = '';
+
+    updateTasks(); // Call the function to update the tasksHTML
 }
+
