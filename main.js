bae7fcc9e4bf9f17e1bf683188ddca16cb017d67
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2023-11-14 14:47:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,68 @@
 
-const tasks = [];
-displayTasks();
+// When loading the page, load from localStorage.
+const todoList = JSON.parse(localStorage.getItem('todoList')) || 
+   [] ;
+  
+  renderTodoList();
+  
+  function renderTodoList() {
+    let todoListHTML = '';
+  
+    for (let i = 0; i < todoList.length; i++) {
+      const todoObject = todoList[i];
+      //const name = todoObject.name;
+      //const dueDate = todoObject.dueDate;
+      const { name,time, dueDate } = todoObject;
+      const html = 
+      `<div class= 'js-tasks'>
+        <div class='task'>${name}</div>
+        <div class='time'>${time}</div>
+        <div class='date'>${dueDate}</div>
+        <button onclick="
+            todoList.splice(${i}, 1);
+            renderTodoList();
 
-function displayTasks() {
-    let tasksHTML = '';
-
-    function updateTasks() {
-        document.querySelector('.js-todolist').innerHTML = tasksHTML;
+            // Whenever we update the todo list, save in localStorage.
+            saveToStorage();
+        " class="delete-todo-button">
+            Delete
+        </button> 
+      </div>
+      `;
+      todoListHTML += html;
     }
+  
+    document.querySelector('.js-todo-list')
+      .innerHTML = todoListHTML;
+  }
+  
+  function addTodo() {
+    const nameInputElement = document.querySelector('.js-name-input');
+    const name = nameInputElement.value;
 
-    for (let i = 0; i < tasks.length; i++) {
-        const toDo = tasks[i];
-        tasksHTML += `<p>${toDo}</p>`;
-    }
-
-    updateTasks();
-}
-
-function addToDo() {
-    const taskInput = document.querySelector('.input-value');
-    const name = taskInput.value;
-
-    tasks.push(name);
-    console.log(tasks);
-
-    taskInput.value = '';
-
-    displayTasks();
-}
+    const timeInputElement = document.querySelector('.js-time-input');
+    const time = timeInputElement.value
+  
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+  
+    todoList.push({
+      //name: name,
+      //dueDate: dueDate,
+      name,
+      time,
+      dueDate
+    });
+  
+    nameInputElement.value = '';
+  
+    renderTodoList();
+  
+    // Whenever we update the todo list, save in localStorage.
+    saveToStorage();
+  }
+  
+  function saveToStorage() {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }
 
